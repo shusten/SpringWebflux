ec24fa86628833271ca65ccb586b75d6fce50517
Luis Villas-Boas <shusten@hotmail.com>
2024-09-30 10:31:03 -0300

    🎉 First commit

---
+++ src/test/java/com/WebfluxFeignStarwars/reactiveApi/ReactiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebfluxFeignStarwars.reactiveApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
